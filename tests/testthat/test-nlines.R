cea88a2c4902fffc20774140878a3c850d3d68c9
Florian <florian.prive.21@gmail.com>
2018-07-24 16:33:06 +0200

    add test for nlines

---
+++ tests/testthat/test-nlines.R
@@ -0,0 +1,12 @@
+context("test-nlines.R")
+
+test_that("'nlines' works", {
+  SEQ <- seq_len(nrow(iris))
+  strings <- c("", "", " ", sapply(10^(seq(0, 4, by = 0.2)), function(i) {
+    paste(as.matrix(iris)[sample(SEQ, i, TRUE), ], collapse = " ")
+  }))
+  replicate(10, {
+    writeLines(sample(strings, replace = TRUE), tmp <- tempfile())
+    expect_identical(nlines(tmp), 24)
+  })
+})
