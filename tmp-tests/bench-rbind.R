5fd3854a06d0f226141a09e2ca578305e52fbd0f
Florian <florian.prive.21@gmail.com>
2018-07-26 23:58:41 +0200

    idem

--- tmp-tests/bench-rbind.R
+++ tmp-tests/bench-rbind.R
@@ -5,8 +5,7 @@ mtcars_dt <- data.table::as.data.table(mtcars)
 list_mtcars <- rep(list(mtcars), 10)
 list_mtcars_dt <- rep(list(mtcars_dt), 10)
 
-rbind_df <- function(...) {
-  list_df <- list(...)
+rbind_df <- function(list_df) {
   list_df_merged <- lapply(seq_along(list_df[[1]]), function(k) {
     unlist(lapply(list_df, function(l) l[[k]]))
   })
@@ -14,25 +13,25 @@ rbind_df <- function(...) {
   as.data.frame(list_df_merged_named, stringsAsFactors = FALSE)
 }
 
-rbind_df2 <- function(...) {
-  data.table::rbindlist(list(...))
+rbind_df2 <- function(list_df) {
+  data.table::rbindlist(list_df)
 }
 
 microbenchmark::microbenchmark(
 
-  A1 = rbind.data.frame(mtcars, mtcars),
-  A2 = rbind.data.frame(mtcars_dt, mtcars_dt),
-  B1 = rbind_df(mtcars, mtcars),
-  B2 = rbind_df(mtcars_dt, mtcars_dt),
-  C1 = rbind_df2(mtcars, mtcars),
-  C2 = rbind_df2(mtcars_dt, mtcars_dt),
+  A1 = rbind.data.frame(mtcars),
+  A2 = rbind.data.frame(mtcars_dt),
+  B1 = rbind_df(list(mtcars)),
+  B2 = rbind_df(list(mtcars_dt)),
+  C1 = rbind_df2(list(mtcars)),
+  C2 = rbind_df2(list(mtcars_dt)),
 
   AA1 = do.call(rbind.data.frame, list_mtcars),
   AA2 = do.call(rbind.data.frame, list_mtcars_dt),
-  BB1 = do.call(rbind_df, list_mtcars),
-  BB2 = do.call(rbind_df, list_mtcars_dt),
-  CC1 = do.call(rbind_df2, list_mtcars),
-  CC2 = do.call(rbind_df2, list_mtcars_dt),
+  BB1 = rbind_df(list_mtcars),
+  BB2 = rbind_df(list_mtcars_dt),
+  CC1 = rbind_df2(list_mtcars),
+  CC2 = rbind_df2(list_mtcars_dt),
 
   times = 10
 )
