9e85a13e974b40ae4ebd7a8fce0d7ca4dcd9c956
Florian <florian.prive.21@gmail.com>
2018-07-28 04:56:02 +0200

    add test

--- tests/testthat/test-split.R
+++ tests/testthat/test-split.R
@@ -11,14 +11,19 @@ test_that("'split_file' works", {
   }))
   for (every in c(1, 2, 4, 12, 24, 25)) {
     writeLines(sample(strings, replace = TRUE), tmp <- tempfile())
+    # Infos are correct
     infos <- split_file(tmp, every, tmp2 <- tempfile())
-    files <- get_split_files(infos)
-    expect_true(all(file.exists(files)))
     expect_identical(infos[["name_in"]], tmp)
     expect_identical(infos[["prefix_out"]], tmp2)
     expect_equal(ceiling(infos[["nlines_all"]] / infos[["nlines_part"]]),
                  infos[["nfiles"]])
     expect_identical(infos[["nlines_all"]], 24L)
+    # New files all exist
+    files <- get_split_files(infos)
+    expect_true(all(file.exists(files)))
+    # Number of lines and size is summing to whole input file
+    expect_identical(sum(sapply(files, nlines)), nlines(tmp))
+    expect_identical(sum(file.size(files)), file.size(tmp))
   }
 })
 
